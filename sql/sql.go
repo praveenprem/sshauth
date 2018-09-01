42170594d581f9551fce75a8e0b4390c4f8253d8
Praveen Premaratne <praveen.mayantha@gmail.com>
2018-09-01 19:51:33 +0100

    Initial SQL package added

---
+++ sql/sql.go
@@ -0,0 +1,14 @@
+package sql
+
+import "github.com/praveenprem/sshauth/config"
+
+/**
+ * Package sql
+ * Project sshauth
+ * Created by Praveen Premaratne
+ * Created on 23/08/2018 21:33
+ */
+
+func Init(username string, publicKey string, conf config.Conf) string {
+	return ""
+}
